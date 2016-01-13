f2e08cb8603e1d6a3907658cfae572ae059dada0
Jhirley <jhirley.fonte@gmail.com>
2016-01-13 18:02:44 -0500

    gulp and demoapp files ad added

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'public/**/*.js', '!public/vendor/**/*.js', 'server/**/*.js'];
+
+gulp.task('style', function () {
+    gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                             './public/js/*.js'], {
+        read: false
+    });
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+    var options = {
+        verbose: true,
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.ejs')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('nodemon', ['style', 'inject'], function () {
+    var options = {
+        script: 'server.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3030,
+            'NODE_ENV': 'development'
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart', ['style'], function (ev) {
+            console.log('Restarting .....');
+            console.log('File changed on restart:\n' + ev);
+        })
+        .on('start', function (ev) {console.log('Starting .....');})
+        .on('crash', function (ev) {console.log('CRASH  .....');})
+        .on('exit', function (ev) {console.log('Clean exit .....');})
+    ;
+});

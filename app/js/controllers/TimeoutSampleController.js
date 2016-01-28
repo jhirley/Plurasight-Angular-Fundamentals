530286d79fd06ae44268078facd1730f375f8292
Jhirley <jhirley.fonte@gmail.com>
2016-01-27 22:34:52 -0500

    Angular Services:  checkpoint

---
+++ app/js/controllers/TimeoutSampleController.js
@@ -0,0 +1,13 @@
+eventsApp.controller('TimeoutSampleController', 
+	function TimeoutSampleController($scope, $interval) {
+		$scope.name = 0;
+		var promise = $interval(function (){
+				$scope.name++;
+			}, 1000*1
+		);
+
+		$scope.cancel = function() {
+			$interval.cancel(promise);
+		}
+	}
+);

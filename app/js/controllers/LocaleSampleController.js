b9ded664dec81e973169c522f26be5b5cfa32d18
Jhirley <jhirley.fonte@gmail.com>
2016-01-27 21:35:21 -0500

    Angular Services:  checkpoint

---
+++ app/js/controllers/LocaleSampleController.js
@@ -0,0 +1,6 @@
+eventsApp.controller('LocaleSampleController', 
+	function LocaleSampleController($scope, $locale) {
+		$scope.myDate = Date.now();
+		$scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+	}
+);

5e090fcd6b48b718e28a60198f1da09d5a097b02
Jhirley <jhirley.fonte@gmail.com>
2016-01-31 21:08:07 -0500

    Angular Services:  checkpoint

---
+++ app/js/controllers/FilterSampleController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+eventsApp.controller('FilterSampleController', 
+	function FilterSampleController($scope, durationFilter) {
+
+		$scope.data = {};
+
+		// var durations = $filter('duration');
+
+		$scope.data.duration1 = durationFilter(1);
+		$scope.data.duration2 = durationFilter(2);
+		$scope.data.duration3 = durationFilter(3);
+		$scope.data.duration4 = durationFilter(4);
+		
+	}
+);
